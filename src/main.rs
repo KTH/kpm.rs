c773a71c39b8e3c1e8324b4401e875232be86432
Johan Öhman Saldes <johan.saldes@gmail.com>
2020-09-15 16:06:09 +0200

    added _monitor endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ async fn main() -> Result<(), std::io::Error> {
     tide::log::start();
     let mut app = tide::new();
     app.at("/kpm/").get(|_| async { Ok("Hello, world!") });
+    app.at("/kpm/_monitor").get(|_| async { Ok("APPLICATION_STATUS: OK\n") });
     app.listen("0.0.0.0:8080").await?;
     Ok(())
 }
