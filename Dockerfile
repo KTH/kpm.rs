cf31ba116d064d47f80b5573c97bceff23ffd15c
Johan Öhman Saldes <johan.saldes@gmail.com>
2020-09-15 17:06:34 +0200

    added a html response

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,5 @@ RUN cargo install --path .
 FROM debian:buster-slim
 # RUN apt-get update && apt-get install -y extra-runtime-dependencies && rm -rf /var/lib/apt/lists/*
 COPY --from=builder /usr/local/cargo/bin/kpm /usr/local/bin/kpm
+COPY templates templates
 CMD ["kpm"]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use tera::Tera;
+use tide_tera::prelude::*;
+use tide::Response;
+use tide::Request;
+
+async fn start_page(req:Request<Tera>) -> Result<Response, tide::Error> {
+    let tera = req.state();
+    tera.render_response("index.html", &context! {})
+}
+
 #[async_std::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tide::log::start();
-    let mut app = tide::new();
-    app.at("/kpm/").get(|_| async { Ok("Hello, world!") });
+
+    let mut tera = Tera::new("templates/**/*")?;
+    tera.autoescape_on(vec!["html"]);
+
+    let mut app = tide::with_state(tera);
+
+    app.at("/kpm/").get(start_page);
     app.at("/kpm/_monitor").get(|_| async { Ok("APPLICATION_STATUS: OK\n") });
     app.listen("0.0.0.0:8080").await?;
     Ok(())
