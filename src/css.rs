a229bb843f83ca54a7663c949f260ba9ccba6bcc
Rasmus Kaj <kaj@kth.se>
2020-09-17 21:48:15 +0200

    Improve style (a little).

--- src/css.rs
+++ src/css.rs
@@ -1,14 +1,21 @@
 use md5;
 
 pub static CSS: &str = "
-body{
-    margin: 2rem;
+html, body {
+    scroll-padding-top: $toolbarheight;
 }
-#kpm>.container{
+body{
+    margin-top: 2rem;
+}
+nav#kpm {
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
+}
+nav#kpm>.container{
+    line-height: 2rem;
+    margin: 0;
     height: 2rem;
     background: peru;
     color: white;
