8d97f685ece2aa054e3af32e682ac3dedd5534fd
Emil <esandin@gmail.com>
2020-09-17 14:06:42 +0200

    rename toolbar.js -> index.js

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut app = tide::with_state(tera);
 
     app.at("/kpm/").get(start_page);
-    app.at("/kpm/toolbar.js").get(toolbar_js);
+    app.at("/kpm/index.js").get(index_js);
     app.at("/kpm/_monitor")
         .get(|_| async { Ok("APPLICATION_STATUS: OK\n") });
     app.listen("0.0.0.0:8080").await?;
@@ -24,8 +24,8 @@ async fn start_page(req: Request<Tera>) -> Result<Response, tide::Error> {
     tera.render_response("index.html", &context! {})
 }
 
-async fn toolbar_js(req: Request<Tera>) -> Result<Response, tide::Error> {
+async fn index_js(req: Request<Tera>) -> Result<Response, tide::Error> {
     let tera = req.state();
-    tera.render_response("toolbar.js", &context! {})
+    tera.render_response("index.js", &context! {})
 }
 
