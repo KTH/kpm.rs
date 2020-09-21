979473d2c8576f23adb72031eb297e3b53d15628
Rasmus Kaj <kaj@kth.se>
2020-09-21 12:50:03 +0200

    Open a mock menu panel.

--- src/css.rs
+++ src/css.rs
@@ -14,14 +14,30 @@ nav#kpm {
     right: 0;
     z-index: 9999;
 }
+nav#kpm.open {
+    height: 100vh;
+    background: rgba(0,0,0,0.3);o
+}
 nav#kpm>.kpmbar{
     line-height: 2rem;
     margin: 0;
-    padding: 0 1rem;
+    padding: 0 max(calc(50vw - 590px), 1rem);
     height: 2rem;
     background: peru;
     color: white;
     border-bottom: solid 1px #fff;
+}
+nav#kpm>.kpmpanel{
+    border: solid 1px peru;
+    border-width: 0 1px 1ex 1px;
+    margin: -1px max(calc(50vw - 590px), 1rem) 0;
+    max-width: 50em;
+    background: white;
+    padding: 1em;
+    display: none;
+}
+nav#kpm.open>.kpmpanel{
+    display: block;
 }\n";
 
 pub fn hash() -> String {
