9938dabd093350c6def6cd4366ecf38b790fbc13
Rasmus Kaj <kaj@kth.se>
2020-10-05 12:40:09 +0200

    Add a button to enable/disable KPM
    With the button there is some text about the current state and how to
    get back.
    
    The actual setting/removal of a cookie is done by a POST request to
    the server, so that the server can log the change.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use async_std::process::exit;
 use httpdate::fmt_http_date;
+use serde::Deserialize;
 use std::env;
 use std::time::{Duration, SystemTime};
 use tera::Tera;
-use tide::http::headers::EXPIRES;
+use tide::http::headers::{EXPIRES, LOCATION, SET_COOKIE};
 use tide::http::mime;
 use tide::{Request, Response};
 use tide_tera::prelude::*;
@@ -15,7 +16,7 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 
 /// The main entry point.
 ///
-/// This just initalizies the loggr, calls run and logs when it returns.
+/// This just initalizies the logger, calls run and logs when it returns.
 #[async_std::main]
 async fn main() {
     tide::log::start();
@@ -38,6 +39,7 @@ async fn run() -> Result<(), Error> {
     let mut app = tide::with_state(State::new()?);
 
     app.at("/kpm/").get(start_page);
+    app.at("/kpm/").post(enable_or_disable);
     app.at("/kpm/index.js").get(index_js);
     app.at(&format!("/kpm/{}", css::page_css_name()))
         .get(page_css);
@@ -62,17 +64,50 @@ impl State {
         let footer = footer::Footer::new();
         Ok(State { tera, footer })
     }
+    fn base_url(&self) -> String {
+        let host_url = env_or("SERVER_HOST_URL", "http://localdev.kth.se:8080");
+        format!("{}/kpm/", host_url)
+    }
 }
 
 async fn start_page(req: Request<State>) -> Result<Response, tide::Error> {
     let kpm = req.state();
+    let is_active = dbg!(req.cookie("use_kpm")).is_some();
     kpm.tera.render_response(
         "index.html",
         &context! {
+            "is_active" => is_active,
             "page_css" => css::page_css_name(),
             "kth_footer" => *kpm.footer.get().await,
         },
     )
+}
+
+async fn enable_or_disable(mut req: Request<State>) -> Result<Response, tide::Error> {
+    let post: StatusForm = req.body_form().await?;
+    let kpm = req.state();
+    let activate = dbg!(post).action == "enable";
+    if activate {
+        tide::log::info!("A user activated KPM");
+    } else {
+        tide::log::info!("A user disabled KPM");
+    }
+    Ok(Response::builder(302)
+        .header(LOCATION, kpm.base_url())
+        .header(
+            SET_COOKIE,
+            if activate {
+                "use_kpm=t; Domain=.kth.se; Path=/; HttpOnly"
+            } else {
+                "use_kpm=; Max-Age=0; Domain=.kth.se; Path=/; HttpOnly"
+            },
+        )
+        .build())
+}
+
+#[derive(Debug, Deserialize)]
+struct StatusForm {
+    action: String,
 }
 
 async fn monitor(_req: Request<State>) -> Result<Response, tide::Error> {
@@ -87,11 +122,12 @@ async fn monitor(_req: Request<State>) -> Result<Response, tide::Error> {
 
 async fn index_js(req: Request<State>) -> Result<Response, tide::Error> {
     let kpm = req.state();
-    let host_url = env_or("SERVER_HOST_URL", "http://localdev.kth.se:8080");
+    let base_url = kpm.base_url();
     kpm.tera.render_response(
         "index.js",
         &context! {
-            "css_url" => format!("{}/kpm/{}", host_url, css::menu_css_name()),
+            "css_url" => format!("{}{}", base_url, css::menu_css_name()),
+            "kpm_base" => base_url,
         },
     )
 }
